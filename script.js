0e5f4723b3123013e0611dd76a448959429f1982
Harpreet Singh <harpreetsinghsodi152@gmail.com>
2022-05-04 22:33:14 +0530

    v5

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+//DOM ELEMENTS
+const sidebar = document.querySelector(".sidebar");
+const form = document.querySelector(".workout__form");
+const formSubmit = document.querySelector(".form__button");
+const formDistance = document.querySelector(".form__distance");
+const workoutType = document.querySelector(".form__select");
+
+let map, mapEvent;
 ////////////////////////////////
 ///////////////////////////////
 //  CASE 1 : Geolocation API Function
@@ -22,16 +30,21 @@ if (navigator.geolocation) {
       ////////////////////////////////
       ///////////////////////////////
       const coords = [latitude, longitude];
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on("click", function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng]).addTo(map).bindPopup(L.popup({})).openPopup();
+      map.on("click", function (e) {
+        //IMPORTANT EVENT FX OF MAP
+
+        form.classList.remove("hidden");
+        formDistance.focus();
+        sidebar.style.setProperty("opacity", "100%");
+
+        mapEvent = e;
       });
     },
     function () {
@@ -39,3 +52,36 @@ if (navigator.geolocation) {
     }
   );
 }
+
+// adding marker on the map as the form submits
+formSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  sidebar.style.removeProperty("opacity");
+  form.classList.toggle("hidden");
+
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeButton: false,
+        closeOnEscapeKey: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("WORKOUT")
+    .openPopup();
+
+  document.querySelector(".form").reset();
+});
+
+//changing credence to elve
+workoutType.addEventListener("change", function (e) {
+  document.querySelector(".cadence").classList.toggle("hidden");
+  document.querySelector(".elve").classList.toggle("hidden");
+});
