5c9602c3cf8ff55380a1f68b65a3cc7d26a199d1
Harpreet Singh <harpreetsinghsodi152@gmail.com>
2022-05-05 16:09:08 +0530

    v5

--- script.js
+++ script.js
@@ -7,81 +7,97 @@ const formSubmit = document.querySelector(".form__button");
 const formDistance = document.querySelector(".form__distance");
 const workoutType = document.querySelector(".form__select");
 
-let map, mapEvent;
-////////////////////////////////
-///////////////////////////////
-//  CASE 1 : Geolocation API Function
-////////////////////////////////
-///////////////////////////////
+class App {
+  #map;
+  #mapEvent; //private instance class fields
 
-/**
- * Checking if the geolocation api is supported or not for older devices
- * the navigator.geoloaction api gets two callback functions , 1 is for success and 2nd is for the error
- */
+  constructor() {
+    this._getPosition();
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude, longitude } = position.coords;
+    //DOM EVENT LISTENER
 
-      ////////////////////////////////
-      ///////////////////////////////
-      //  CASE 2 : Using leaflet libarary to display map
-      ////////////////////////////////
-      ///////////////////////////////
-      const coords = [latitude, longitude];
-      map = L.map("map").setView(coords, 13);
+    // adding marker on the map as the form submits
+    formSubmit.addEventListener("click", this._newWorkout.bind(this));
 
-      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    //changing credence to elve
+    workoutType.addEventListener("change", this._toogleElevationFields);
+  }
 
-      map.on("click", function (e) {
-        //IMPORTANT EVENT FX OF MAP
+  _getPosition() {
+    /**
+     * Checking if the geolocation api is supported or not for older devices
+     * the navigator.geoloaction api gets two callback functions , 1 is for success and 2nd is for the error
+     */
 
-        form.classList.remove("hidden");
-        formDistance.focus();
-        sidebar.style.setProperty("opacity", "100%");
-
-        mapEvent = e;
-      });
-    },
-    function () {
-      alert("couldn't connect");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("couldn't connect");
+        }
+      );
     }
-  );
+  }
+
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+
+    ////////////////////////////////
+    ///////////////////////////////
+    //  CASE 2 : Using leaflet libarary to display map
+    ////////////////////////////////
+    ///////////////////////////////
+    const coords = [latitude, longitude];
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(e) {
+    //IMPORTANT EVENT FX OF MAP
+
+    form.classList.remove("hidden");
+    formDistance.focus();
+    sidebar.style.setProperty("opacity", "100%");
+
+    this.#mapEvent = e;
+  }
+
+  _toogleElevationFields() {
+    document.querySelector(".cadence").classList.toggle("hidden");
+    document.querySelector(".elve").classList.toggle("hidden");
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    sidebar.style.removeProperty("opacity");
+    form.classList.toggle("hidden");
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeButton: false,
+          closeOnEscapeKey: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("WORKOUT")
+      .openPopup();
+
+    document.querySelector(".form").reset();
+  }
 }
 
-// adding marker on the map as the form submits
-formSubmit.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  sidebar.style.removeProperty("opacity");
-  form.classList.toggle("hidden");
-
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeButton: false,
-        closeOnEscapeKey: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("WORKOUT")
-    .openPopup();
-
-  document.querySelector(".form").reset();
-});
-
-//changing credence to elve
-workoutType.addEventListener("change", function (e) {
-  document.querySelector(".cadence").classList.toggle("hidden");
-  document.querySelector(".elve").classList.toggle("hidden");
-});
+const app = new App();
